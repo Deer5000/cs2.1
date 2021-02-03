590c72c9c44d483b3bb5826d1f83e2a254b6bcc9
Deer5000 <khidrbrinkley32@gmail.com>
2021-02-02 22:00:09 -0500

    answered questions for merge sort

--- recursiveSort/sorting_recursive.py
+++ recursiveSort/sorting_recursive.py
@@ -17,8 +17,8 @@ def merge(left, right):
 
     """Merge given lists of items, each assumed to already be in sorted order,
     and return a new list containing all items in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n) because it merges n elements.
+    TODO: Memory usage: O(n)"""
     # TODO: Repeat until one list is empty✅
     # TODO: Find minimum item in both lists and append it to new list✅
     # TODO: Append remaining items in non-empty list to new list✅
@@ -37,17 +37,15 @@ def merge_sort(items):
     return merge(left_sort, right_sort)
 
 
-
-
-
     """Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if list is so small it's already sorted (base case)
-    # TODO: Split items list into approximately equal halves
-    # TODO: Sort each half by recursively calling merge sort
-    # TODO: Merge sorted halves into one list in sorted order
+    TODO: Running time: O(logn) because it firsts divides which is O(1) and then it conquers
+    which recursively sorts the two subarrays of n/2 elements each.
+    TODO: Memory usage: O(n)"""
+    # TODO: Check if list is so small it's already sorted (base case) ✅
+    # TODO: Split items list into approximately equal halves ✅
+    # TODO: Sort each half by recursively calling merge sort ✅
+    # TODO: Merge sorted halves into one list in sorted order ✅
 
 
 def partition(items, low, high):
