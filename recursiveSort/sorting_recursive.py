ad764c3201b8a993be40a2e43f2db808beb392f9
Deer5000 <khidrbrinkley32@gmail.com>
2021-02-02 22:11:36 -0500

    quicksort started

--- recursiveSort/sorting_recursive.py
+++ recursiveSort/sorting_recursive.py
@@ -1,3 +1,7 @@
+from random import randrange, shuffle
+
+
+
 def merge(left, right):
     result = []
     while(left and right):
@@ -48,27 +52,47 @@ def merge_sort(items):
     # TODO: Merge sorted halves into one list in sorted order ✅
 
 
-def partition(items, low, high):
-    """Return index `p` after in-place partitioning given items in range
-    `[low...high]` by choosing a pivot (TODO: document your method here) from
-    that range, moving pivot into index `p`, items less than pivot into range
-    `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Choose a pivot any way and document your method in docstring above
-    # TODO: Loop through all items in range [low...high]
-    # TODO: Move items less than pivot into front of range [low...p-1]
-    # TODO: Move items greater than pivot into back of range [p+1...high]
-    # TODO: Move pivot item into final position [p] and return index p
+# def partition(items, low, high):
+#     """Return index `p` after in-place partitioning given items in range
+#     `[low...high]` by choosing a pivot (TODO: document your method here) from
+#     that range, moving pivot into index `p`, items less than pivot into range
+#     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
+#     TODO: Running time: ??? Why and under what conditions?
+#     TODO: Memory usage: ??? Why and under what conditions?"""
+#     # TODO: Choose a pivot any way and document your method in docstring above
+#     # TODO: Loop through all items in range [low...high]
+#     # TODO: Move items less than pivot into front of range [low...p-1]
+#     # TODO: Move items greater than pivot into back of range [p+1...high]
+#     # TODO: Move pivot item into final position [p] and return index p
 
 
-def quick_sort(items, low=None, high=None):
-    """Sort given items in place by partitioning items in range `[low...high]`
-    around a pivot item and recursively sorting each remaining sublist range.
-    TODO: Best case running time: ??? Why and under what conditions?
-    TODO: Worst case running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if high and low range bounds have default values (not given)
-    # TODO: Check if list or range is so small it's already sorted (base case)
-    # TODO: Partition items in-place around a pivot and get index of pivot
-    # TODO: Sort each sublist range by recursively calling quick sort
+def quick_sort(list, start, end):
+  if start >= end:
+    return
+
+  pivot_idx = randrange(start, end + 1)
+  pivot_element = list[pivot_idx]
+  list[end], list[pivot_idx] = list[pivot_idx], list[end]
+
+  less_than_pointer = start
+
+  for i in range(start, end):
+    if list[i] < pivot_element:
+      list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
+      less_than_pointer += 1
+
+  list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
+  quick_sort(list, start, less_than_pointer - 1)
+  quick_sort(list, less_than_pointer + 1, end)
+
+
+
+"""Sort given items in place by partitioning items in range `[low...high]`
+around a pivot item and recursively sorting each remaining sublist range.
+TODO: Best case running time: ??? Why and under what conditions?
+TODO: Worst case running time: ??? Why and under what conditions?
+TODO: Memory usage: ??? Why and under what conditions?
+TODO: Check if high and low range bounds have default values (not given)
+TODO: Check if list or range is so small it's already sorted (base case)
+TODO: Partition items in-place around a pivot and get index of pivot
+TODO: Sort each sublist range by recursively calling quick sort"""
