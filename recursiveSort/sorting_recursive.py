821e0dbbfbed04ae52bd45ae70f90b1818d09412
Deer5000 <khidrbrinkley32@gmail.com>
2021-02-02 22:18:28 -0500

    finished quicksort questions

--- recursiveSort/sorting_recursive.py
+++ recursiveSort/sorting_recursive.py
@@ -52,19 +52,6 @@ def merge_sort(items):
     # TODO: Merge sorted halves into one list in sorted order ✅
 
 
-# def partition(items, low, high):
-#     """Return index `p` after in-place partitioning given items in range
-#     `[low...high]` by choosing a pivot (TODO: document your method here) from
-#     that range, moving pivot into index `p`, items less than pivot into range
-#     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-#     TODO: Running time: ??? Why and under what conditions?
-#     TODO: Memory usage: ??? Why and under what conditions?"""
-#     # TODO: Choose a pivot any way and document your method in docstring above
-#     # TODO: Loop through all items in range [low...high]
-#     # TODO: Move items less than pivot into front of range [low...p-1]
-#     # TODO: Move items greater than pivot into back of range [p+1...high]
-#     # TODO: Move pivot item into final position [p] and return index p
-
 
 def quick_sort(list, start, end):
   if start >= end:
@@ -89,10 +76,6 @@ def quick_sort(list, start, end):
 
 """Sort given items in place by partitioning items in range `[low...high]`
 around a pivot item and recursively sorting each remaining sublist range.
-TODO: Best case running time: ??? Why and under what conditions?
-TODO: Worst case running time: ??? Why and under what conditions?
-TODO: Memory usage: ??? Why and under what conditions?
-TODO: Check if high and low range bounds have default values (not given)
-TODO: Check if list or range is so small it's already sorted (base case)
-TODO: Partition items in-place around a pivot and get index of pivot
-TODO: Sort each sublist range by recursively calling quick sort"""
+TODO: Best case running time: O(n log n)✅
+TODO: Worst case running time: O(n2)✅
+TODO: Memory usage: O(log n)"""✅
